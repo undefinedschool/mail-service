91cdc5942385b0551a4313298d84a27eea570068
nhsz <nh.quiroz@gmail.com>
2020-04-13 10:55:37 -0300

    chore: add body parser

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const router = require('./routes');
 const { PORT = 8080 } = process.env;
 const app = express();
 
-app.use(helmet());
 app.use(express.json());
+app.use(helmet());
 app.use(router);
 
 const server = app.listen(PORT);
